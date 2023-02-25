4621bbb6d4829ed40597cc4b15aca5e17abcd622
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-02-26 00:04:12 +0300

    Completed

---
+++ Calculator/Controllers/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var isFinishedTypingNumber: Bool = true
+    private var calculator = CalculatorLogic()
+    private var displayValue: Double {
+        
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label to Double.")
+            }
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+        
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+                
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                    
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+    
+    }
+
+}
+
